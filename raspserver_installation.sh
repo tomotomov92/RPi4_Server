e025828bbdd1049d1d1df2ea8effa8733e261c10
Tomo Tomov <48110573+tomotomov92@users.noreply.github.com>
2024-06-29 15:11:37 +0300

    Update raspserver_installation.sh

--- raspserver_installation.sh
+++ raspserver_installation.sh
@@ -1,35 +1,31 @@
-#Change hostname and timezone:
+## Change hostname and timezone:
 sudo raspi-config nonint do_hostname raspserver
 sudo timedatectl set-timezone Europe/Sofia
 
-#Create directories and give tomo permissions:
+## Create the necessary directories and permissions
 sudo mkdir -p /backups
 sudo mkdir -p /backup_temp
 sudo mkdir -p /docker
-
-#Change owner
 sudo chown -R tomo:tomo /backups
 sudo chown -R tomo:tomo /backup_temp
 sudo chown -R tomo:tomo /docker
+sudo chmod -R 600 /backups
+sudo chmod -R 600 /backup_temp
+sudo chmod -R 600 /docker
 
-#Add permissions to directories
-sudo chmod -R 777 /backups
-sudo chmod -R 777 /backup_temp
-sudo chmod -R 777 /docker
+## Update apt
+sudo apt update && sudo apt upgrade -y
 
-#Install required software from apt:
-sudo apt update
-sudo apt upgrade
-sudo apt install docker docker-compose -y
+## Install Docker
+curl -sSL https://get.docker.com | sh
+## Add user to docker group
+sudo usermod -aG docker $USER
 
+## Install Tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscale set --advertise-exit-node
 sudo tailscale up --advertise-routes=192.168.2.0/24 --ssh --advertise-exit-node
 
-#Add permissions for default user to docker commands:
-sudo usermod -aG docker tomo
-
-sudo reboot
 
 #Add cron task for backing up every two days:
 crontab -e
