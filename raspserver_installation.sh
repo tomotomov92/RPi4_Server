84c7b73872659a4af60db3f261e4ec1103ee5c32
Tomo Tomov <48110573+tomotomov92@users.noreply.github.com>
2024-11-06 13:06:26 +0200

    Update raspserver_installation.sh

--- raspserver_installation.sh
+++ raspserver_installation.sh
@@ -28,7 +28,7 @@ echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
 sudo tailscale set --advertise-exit-node
-sudo tailscale up --accept-routes --advertise-exit-node --advertise-routes=192.168.2.0/24,192.168.1.0/24 --ssh
+sudo tailscale up --advertise-exit-node --advertise-routes=192.168.2.0/24,192.168.1.0/24 --ssh
 
 #Add cron task for backing up every two days:
 crontab -e
